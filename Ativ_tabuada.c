9e35efba8d34da61cede9e569cad520eb1f69343
LueSantos <86605122+LueSantos@users.noreply.github.com>
2021-08-04 12:03:32 -0300

    ("Tabuada");

--- Ativ_tabuada.c
+++ Ativ_tabuada.c
@@ -3,9 +3,8 @@
  Name        : Ativ_tabuada.c
  Author      : Luciana Santos
 
-2. Fazer um programa em C que imprime uma tabela com a tabuada de 1 a 9
+2. Fazer um programa em C que imprime uma tabela com a tabuada de 1 a 9.
 
- Description : Hello World in C, Ansi-style
  ============================================================================
  */
 
